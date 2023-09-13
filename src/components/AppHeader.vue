477db87b90cfe7d3f3b482a57bc7dc8d4068ff4a
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-13 16:30:53 +0200

    feat: select

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,6 +10,13 @@ export default {
 
 <template>
   <h1><font-awesome-icon icon="fa-solid fa-user-secret" />{{ title }}</h1>
+  <!-- SELECT -->
+  <select class="form-select" aria-label="Default select example">
+    <option selected>Open this select menu</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
 </template>
 
 <style lang="scss" scoped></style>
