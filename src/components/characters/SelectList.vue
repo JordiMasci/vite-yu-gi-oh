0afc3a7e476d6f1068334b842fcc06078d91e5f9
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-13 17:28:18 +0200

    feat: select responsive

--- src/components/characters/SelectList.vue
+++ src/components/characters/SelectList.vue
@@ -1,16 +1,50 @@
 <script>
+import axios from "axios";
+
 export default {
-  emits: []
+  data() {
+    return {
+      types: [],
+      currentType: "",
+    };
+  },
+
+  methods: {
+    fetchTypes() {
+      axios
+        .get("https://db.ygoprodeck.com/api/v7/archetypes.php")
+        .then((res) => {
+          this.types = res.data;
+          console.log(res.data);
+        });
+    },
+    changeType() {
+        console.log(this.currentType)
+    },
+  },
+
+  created() {
+    this.fetchTypes();
+  },
+
 };
 </script>
 
 <template>
   <!-- SELECT -->
-  <select class="form-select" aria-label="Default select example">
-    <option selected>Open this select menu</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
+  <select
+    @change="changeType()"
+    v-model="currentType"
+    class="form-select"
+    aria-label="Default select example"
+  >
+    <option
+      v-for="(type, index) in types"
+      :value="type.archetype_name"
+      :key="index"
+    >
+      {{ type.archetype_name }}
+    </option>
   </select>
 </template>
 
